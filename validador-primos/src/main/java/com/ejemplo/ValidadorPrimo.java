5fc5e86a19b6f14ec0115eff89d0eff5ff43f91d
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2025-05-21 01:35:58 +0000

    .

--- validador-primos/src/main/java/com/ejemplo/ValidadorPrimo.java
+++ validador-primos/src/main/java/com/ejemplo/ValidadorPrimo.java
@@ -2,6 +2,10 @@ package com.ejemplo;
 
 public class ValidadorPrimo {
     public static boolean esPrimo(int numero) {
-        return false;
+        if (numero <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) return false;
+        }
+        return true;
     }
 }
--- validador-primos/src/test/java/com/ejemplo/ValidadorPrimoTest.java
+++ validador-primos/src/test/java/com/ejemplo/ValidadorPrimoTest.java
@@ -10,4 +10,104 @@ public class ValidadorPrimoTest {
         // 🚨 Falla porque ValidadorPrimo aún no existe
         assertFalse(ValidadorPrimo.esPrimo(1));
     }
+
+    @Test
+    public void testDosEsPrimo() {
+    assertTrue(ValidadorPrimo.esPrimo(2));
+    }
+
+    @Test
+    public void testTresEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(3));
+    }
+
+    @Test
+    public void testCincoEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(5));
+    }
+
+@Test 
+    public void testVeintitresEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(23));
+    }
+
+    @Test
+    public void testVeintinueveEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(29));
+    }
+
+    @Test
+    public void testTreintaYUnoEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(31));
+    }
+
+    @Test
+    public void testTreintaYSeisEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(37));
+    }
+
+    @Test
+    public void testCuarentaYUnoEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(41));
+    }
+
+    @Test
+    public void testCuarentaYTresEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(43));
+    }
+
+    @Test
+    public void testCuarentaYSieteEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(47));
+    }
+
+    @Test
+    public void testCincuentaYTresEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(53));
+    }
+
+    @Test
+    public void testCincuentaYNueveEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(59));
+    }
+
+    @Test
+    public void testSesentaYUnoEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(61));
+    }
+
+    @Test
+    public void testSesentaYSieteEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(67));
+    }
+
+    @Test
+    public void testSetentaYUnoEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(71));
+    }
+
+    @Test
+    public void testSetentaYTresEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(73));
+    }
+
+    @Test
+    public void testSetentaYNueveEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(79));
+    }
+
+    @Test
+    public void testOchentaYTresEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(83));
+    }
+
+    @Test
+    public void testOchentaYNueveEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(89));
+    }
+
+    @Test
+    public void testNoventaYSieteEsPrimo() {
+        assertTrue(ValidadorPrimo.esPrimo(97));
+    }
 }
